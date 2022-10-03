c2acb945eaa088d0a31a65a4bddaa326e68776f8
Pavel Anokhin <anohinpa@gmail.com>
2022-10-03 20:05:09 +0300

    Task 34

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// 
+﻿// Task 34
+
+Console.WriteLine("Введите количество элементов массива: ");
+int [] array = new int[Convert.ToInt32(Console.ReadLine())];
+
+int evenCount = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    Console.Write($"{array[i]} ");
+    if (array[i] % 2 == 0) evenCount++;
+}
+Console.WriteLine();
+Console.WriteLine(evenCount);
