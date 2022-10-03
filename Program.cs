89a59492de579b5cd3d66b320ff27d8ee605c1fa
Pavel Anokhin <anohinpa@gmail.com>
2022-10-03 20:22:45 +0300

    Task 34

--- Program.cs
+++ Program.cs
@@ -3,13 +3,13 @@
 Console.WriteLine("Введите количество элементов массива: ");
 int [] array = new int[Convert.ToInt32(Console.ReadLine())];
 
-int evenCount = 0;
+int sum = 0;
 
 for (int i = 0; i < array.Length; i++)
 {
-    array[i] = new Random().Next(100, 1000);
+    array[i] = new Random().Next(-100, 100);
     Console.Write($"{array[i]} ");
-    if (array[i] % 2 == 0) evenCount++;
+    if (i % 2 == 1) sum += array[i];
 }
 Console.WriteLine();
-Console.WriteLine(evenCount);
+Console.WriteLine(sum);
