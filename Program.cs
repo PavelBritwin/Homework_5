525e1c94cd3600f8afe6971a807334a0277d0d2f
Pavel Anokhin <anohinpa@gmail.com>
2022-10-03 20:38:32 +0300

    Task38

--- Program.cs
+++ Program.cs
@@ -1,15 +1,17 @@
-﻿// Task 36
+﻿// Task 38
 
 Console.WriteLine("Введите количество элементов массива: ");
 int [] array = new int[Convert.ToInt32(Console.ReadLine())];
 
-int sum = 0;
+int min = 99;
+int max = 0;
 
 for (int i = 0; i < array.Length; i++)
 {
-    array[i] = new Random().Next(-100, 100);
+    array[i] = new Random().Next(0, 100);
     Console.Write($"{array[i]} ");
-    if (i % 2 == 1) sum += array[i];
+    if (min > array[i]) min = array[i];
+    if (max < array[i]) max = array[i];
 }
 Console.WriteLine();
-Console.WriteLine(sum);
+Console.WriteLine(max - min);
